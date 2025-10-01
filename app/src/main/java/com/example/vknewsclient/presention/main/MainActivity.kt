214a35d0ae1fcb4a7571dda259bb92379be0d827
meeten <153901120+meeten@users.noreply.github.com>
2025-10-01 19:14:09 +0200

    feat: add application class and improve package structure - Добавлен слой app для основной логики приложения - Добавлен класс App, унаследованный от Application - Подписан класс App в AndroidManifest - Класс MainActivity перемещен в слой presentation

--- app/src/main/java/com/example/vknewsclient/presention/main/MainActivity.kt
+++ app/src/main/java/com/example/vknewsclient/presention/main/MainActivity.kt
@@ -1,16 +1,8 @@
-package com.example.vknewsclient
+package com.example.vknewsclient.presention.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.vknewsclient.ui.theme.VkNewsClientTheme
 
 class MainActivity : ComponentActivity() {
