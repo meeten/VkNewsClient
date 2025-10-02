2b1447ee97f1f6fc517f457735ccc5f9bfff6ee4
meeten <153901120+meeten@users.noreply.github.com>
2025-10-02 13:22:31 +0200

    feat: add authentication logic in view model - Добавлено свойство отвечающее за состояние аутентификации - Добавлен метод проверяющий текущий токен аутентификации - Добавлен метод для запуска процесса регистрации через VK ID

--- app/src/main/java/com/example/vknewsclient/presention/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/vknewsclient/presention/auth/AuthViewModel.kt
@@ -1,7 +1,77 @@
 package com.example.vknewsclient.presention.auth
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.vknewsclient.domain.models.AuthState
+import com.vk.id.AccessToken
 import com.vk.id.VKID
+import com.vk.id.VKIDAuthFail
+import com.vk.id.auth.VKIDAuthCallback
+import com.vk.id.refresh.VKIDRefreshTokenCallback
+import com.vk.id.refresh.VKIDRefreshTokenFail
+import com.vk.id.refresh.VKIDRefreshTokenFail.FailedApiCall
+import com.vk.id.refresh.VKIDRefreshTokenFail.FailedOAuthState
+import com.vk.id.refresh.VKIDRefreshTokenFail.NotAuthenticated
+import com.vk.id.refresh.VKIDRefreshTokenFail.RefreshTokenExpired
+import kotlinx.coroutines.launch
 
-class AuthViewModel(vkid: VKID) : ViewModel() {
+class AuthViewModel(val vkid: VKID) : ViewModel() {
+    private val _authState = MutableLiveData<AuthState>()
+    val authState: LiveData<AuthState> get() = _authState
+
+    init {
+        checkToken()
+    }
+
+    private fun checkToken() {
+        viewModelScope.launch {
+            vkid.refreshToken(callback = object : VKIDRefreshTokenCallback {
+                override fun onSuccess(token: AccessToken) {
+                    _authState.value = AuthState.Authenticated(token)
+                }
+
+                override fun onFail(fail: VKIDRefreshTokenFail) {
+                    when (fail) {
+                        is FailedApiCall -> {
+                            _authState.value =
+                                AuthState.Error(fail.description)
+                        }
+
+                        is FailedOAuthState -> {
+                            _authState.value =
+                                AuthState.Error(fail.description)
+                        }
+
+                        is RefreshTokenExpired -> {
+                            _authState.value =
+                                AuthState.NoAuthenticated
+                        } // Это означает, что нужно пройти авторизацию заново
+
+                        is NotAuthenticated -> {
+                            _authState.value = AuthState.NoAuthenticated
+                        } // Пользователь понимает, что сначала нужно авторизоваться
+                    }
+                }
+            })
+
+        }
+    }
+
+    fun registration() {
+        val vkidAuthCallback = object : VKIDAuthCallback {
+            override fun onAuth(accessToken: AccessToken) {
+                _authState.value = AuthState.Authenticated(accessToken)
+            }
+
+            override fun onFail(fail: VKIDAuthFail) {
+                _authState.value = AuthState.Error(fail.description)
+            }
+        }
+
+        viewModelScope.launch {
+            vkid.authorize(vkidAuthCallback)
+        }
+    }
 }
