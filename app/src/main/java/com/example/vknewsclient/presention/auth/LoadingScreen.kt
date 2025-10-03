f10f226f87d14309f84ad024a250ea18a6ce1e3b
meeten <153901120+meeten@users.noreply.github.com>
2025-10-03 17:00:00 +0200

    feat: implement LoadingScreen composable with progress indicator

--- app/src/main/java/com/example/vknewsclient/presention/auth/LoadingScreen.kt
+++ app/src/main/java/com/example/vknewsclient/presention/auth/LoadingScreen.kt
@@ -1,8 +1,23 @@
 package com.example.vknewsclient.presention.auth
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun LoadingScreen() {
-
+    Scaffold(bottomBar = {
+        BottomAppBar {
+            LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
+        }
+    }) {
+        Column(modifier = Modifier.padding(it)) {
+            LoginScreen { }
+        }
+    }
 }
