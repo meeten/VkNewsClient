b7d1bbca2ec37967ec780baa8bd4719e83a1007f
meeten <153901120+meeten@users.noreply.github.com>
2025-10-03 19:37:39 +0200

    feat: add NavigationItem for BottomBarNavigation

---
+++ app/src/main/java/com/example/vknewsclient/presention/main/NavigationItem.kt
@@ -0,0 +1,15 @@
+package com.example.vknewsclient.presention.main
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class NavigationItem(val imageVector: ImageVector, title: String) {
+    object Home : NavigationItem(imageVector = Icons.Outlined.Home, title = "Главная")
+
+    object Favorite : NavigationItem(imageVector = Icons.Outlined.Favorite, title = "Избранное")
+
+    object Profile : NavigationItem(imageVector = Icons.Outlined.Person, title = "Профиль")
+}
