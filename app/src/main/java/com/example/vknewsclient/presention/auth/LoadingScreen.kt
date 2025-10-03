17669fc7f30e559950e8d528175b188983d74303
meeten <153901120+meeten@users.noreply.github.com>
2025-10-03 18:35:00 +0200

    refactor: improve LoadingScreen composable

--- app/src/main/java/com/example/vknewsclient/presention/auth/LoadingScreen.kt
+++ app/src/main/java/com/example/vknewsclient/presention/auth/LoadingScreen.kt
@@ -3,7 +3,6 @@ package com.example.vknewsclient.presention.auth
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -12,9 +11,9 @@ import androidx.compose.ui.Modifier
 @Composable
 fun LoadingScreen() {
     Scaffold(bottomBar = {
-        BottomAppBar {
-            LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
-        }
+        LinearProgressIndicator(
+            modifier = Modifier.fillMaxWidth()
+        )
     }) {
         Column(modifier = Modifier.padding(it)) {
             LoginScreen { }
