a88b3064f8087b5e22a628342f21b714891ed8e0
meeten <153901120+meeten@users.noreply.github.com>
2025-10-03 16:28:40 +0200

    chore: update project configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,12 +19,18 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
+
         maven {
             url = URI("https://artifactory-external.vkpartner.ru/artifactory/vkid-sdk-android/")
         }
         maven {
             url = URI("https://artifactory-external.vkpartner.ru/artifactory/maven/")
         }
+        maven {
+            url =
+                URI("https://artifactory-external.vkpartner.ru/artifactory/vk-id-captcha/android/")
+        }
+
         mavenCentral()
     }
 }
