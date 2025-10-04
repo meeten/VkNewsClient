0ea73a00a4c1157f20f480ecde2c26bfc481cebc
meeten <153901120+meeten@users.noreply.github.com>
2025-10-04 17:30:50 +0200

    refactor: add screen in NavigationItem

--- app/src/main/java/com/example/vknewsclient/presention/main/NavigationItem.kt
+++ app/src/main/java/com/example/vknewsclient/presention/main/NavigationItem.kt
@@ -4,12 +4,19 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.Scaffold
 import androidx.compose.ui.graphics.vector.ImageVector
+import com.example.vknewsclient.navigation.Screen
 
-sealed class NavigationItem(val imageVector: ImageVector, val title: String) {
-    object Home : NavigationItem(imageVector = Icons.Outlined.Home, title = "Главная")
+sealed class NavigationItem(val screen: Screen, val imageVector: ImageVector, val title: String) {
+    object Home :
+        NavigationItem(screen = Screen.Home, imageVector = Icons.Outlined.Home, title = "Главная")
 
-    object Favorite : NavigationItem(imageVector = Icons.Outlined.Favorite, title = "Избранное")
+    object Favorite : NavigationItem(
+        screen = Screen.Favorite, imageVector = Icons.Outlined.Favorite, title = "Избранное"
+    )
 
-    object Profile : NavigationItem(imageVector = Icons.Outlined.Person, title = "Профиль")
+    object Profile : NavigationItem(
+        screen = Screen.Profile, imageVector = Icons.Outlined.Person, title = "Профиль"
+    )
 }
