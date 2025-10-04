70817bd60d51f492bddeccc5d2a0d55c71945b25
meeten <153901120+meeten@users.noreply.github.com>
2025-10-04 17:35:53 +0200

    feat: implement HomeScreen composable

---
+++ app/src/main/java/com/example/vknewsclient/presention/home/HomeScreen.kt
@@ -0,0 +1,11 @@
+package com.example.vknewsclient.presention.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.vknewsclient.presention.home.news.PostCard
+
+@Composable
+fun HomeScreen(modifier: Modifier) {
+    PostCard(modifier = modifier)
+}
+
