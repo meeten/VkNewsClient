a28589889a4a430a19de7192d91e1e7f09da42e2
meeten <153901120+meeten@users.noreply.github.com>
2025-10-04 17:17:09 +0200

    feat: implement NavigationState for bottom bar navigation – Добавить класс NavigationState с методом navigateTo – Реализовать навигацию по нижней панели с одним верхним запуском – Добавить компонуемую функцию rememberNavigationState – Настроить навигацию с восстановлением состояния

---
+++ app/src/main/java/com/example/vknewsclient/navigation/NavigationState.kt
@@ -0,0 +1,27 @@
+package com.example.vknewsclient.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+class NavigationState(val navController: NavHostController) {
+    fun navigateTo(route: String) {
+        navController.navigate(route) {
+            popUpTo(navController.graph.findStartDestination().id) {
+                saveState = true
+            }
+
+            launchSingleTop = true
+            restoreState = true
+        }
+    }
+}
+
+@Composable
+fun rememberNavigationState(
+    navController: NavHostController = rememberNavController(),
+): NavigationState {
+    return remember { NavigationState(navController) }
+}
