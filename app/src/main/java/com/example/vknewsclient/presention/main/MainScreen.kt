09226bc9f2e411d3f5b8bb664a9e13bad49dc7bb
meeten <153901120+meeten@users.noreply.github.com>
2025-10-04 17:34:46 +0200

    feat: integrate navigation to MainScreen – Подключите AppNavGraph к состоянию навигации. – Добавьте тестовые компонуемые элементы для вкладок «Избранное» и «Профиль». – Подключите нижнюю панель к потоку навигации

--- app/src/main/java/com/example/vknewsclient/presention/main/MainScreen.kt
+++ app/src/main/java/com/example/vknewsclient/presention/main/MainScreen.kt
@@ -1,20 +1,37 @@
 package com.example.vknewsclient.presention.main
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import com.example.vknewsclient.presention.news.PostCard
+import com.example.vknewsclient.navigation.AppNavGraph
+import com.example.vknewsclient.navigation.rememberNavigationState
+import com.example.vknewsclient.presention.home.HomeScreen
 
 @Composable
 fun MainScreen() {
+    val navigationState = rememberNavigationState()
+
     Scaffold(bottomBar = {
-        MainNavigationBottomBar()
+        MainNavigationBottomBar(navigationState)
     }) {
-        PostCard(modifier = Modifier.padding(it))
+        AppNavGraph(
+            navController = navigationState.navController,
+            homeContentScreen = { HomeScreen(modifier = Modifier.padding(it)) },
+            favoriteContentScreen = { TestNavigationState("favorite") },
+            profileContentScreen = { TestNavigationState("profile") }
+        )
     }
+}
+
+@Composable
+private fun TestNavigationState(title: String) {
+    val countState = remember() { mutableIntStateOf(0) }
+    Text(text = "$title: click ${countState.intValue}", modifier = Modifier.clickable {
+        countState.intValue += 1
+    })
 }
