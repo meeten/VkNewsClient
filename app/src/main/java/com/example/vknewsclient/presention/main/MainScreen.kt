0b950723189568d72b392c26dc8e4907e4ef70ea
meeten <153901120+meeten@users.noreply.github.com>
2025-10-04 11:01:21 +0200

    feat: add skeleton composable MainScreen composable

--- app/src/main/java/com/example/vknewsclient/presention/main/MainScreen.kt
+++ app/src/main/java/com/example/vknewsclient/presention/main/MainScreen.kt
@@ -1,8 +1,20 @@
 package com.example.vknewsclient.presention.main
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.vknewsclient.presention.news.PostCard
 
 @Composable
 fun MainScreen() {
-
+    Scaffold(bottomBar = {
+        MainNavigationBottomBar()
+    }) {
+        PostCard(modifier = Modifier.padding(it))
+    }
 }
