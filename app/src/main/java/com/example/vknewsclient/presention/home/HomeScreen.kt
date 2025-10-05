2d7cd18316ca12b08d90482c92e5b7e4292ed923
meeten <153901120+meeten@users.noreply.github.com>
2025-10-05 20:04:05 +0200

    feat: add feed functionality to HomeScreen - Реализовать список публикаций с функцией закрытия свайпом - Интегрировать HomeViewModel для управления состоянием - Добавить анимированное удаление публикаций

--- app/src/main/java/com/example/vknewsclient/presention/home/HomeScreen.kt
+++ app/src/main/java/com/example/vknewsclient/presention/home/HomeScreen.kt
@@ -1,11 +1,41 @@
 package com.example.vknewsclient.presention.home
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.vknewsclient.presention.home.news.PostCard
 
 @Composable
 fun HomeScreen(modifier: Modifier) {
-    PostCard(modifier = modifier)
-}
+    val viewModel: HomeViewModel = viewModel()
+    val feedPosts = viewModel.feedPosts.observeAsState(emptyList()).value
 
+    LazyColumn(modifier = modifier) {
+        items(items = feedPosts, key = { it.id }) { feedPost ->
+            val swipeToDismissBoxState = rememberSwipeToDismissBoxState(
+                confirmValueChange = {
+                    if (it == SwipeToDismissBoxValue.EndToStart) {
+                        viewModel.deleteFeedPost(feedPost)
+                        true
+                    }
+
+                    false
+                })
+
+            SwipeToDismissBox(
+                state = swipeToDismissBoxState,
+                backgroundContent = {},
+                modifier = Modifier.animateItem(),
+                enableDismissFromStartToEnd = false,
+            ) {
+                PostCard()
+            }
+        }
+    }
+}
