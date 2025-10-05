0bd3f821e7007606e59db04be9a59ea439a3b24e
meeten <153901120+meeten@users.noreply.github.com>
2025-10-05 19:58:20 +0200

    feat: add FeedPost data model with mock data - Создать класс FeedPost для представления постов - Включить информацию о сообществе, контент и статистику - Добавить генерацию случайных чисел для статистики - Подгтовка макетных данных для тестирования

---
+++ app/src/main/java/com/example/vknewsclient/domain/FeedPost.kt
@@ -0,0 +1,36 @@
+package com.example.vknewsclient.domain
+
+import com.example.vknewsclient.R
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+private fun randomCountInRangeForStatisticItem(range: IntRange): Int {
+    return Random.nextInt(range)
+}
+
+data class FeedPost(
+    val id: Int,
+    val publicImage: Int = R.drawable.post_comunity_thumbnail,
+    val publicName: String = "/dev/null",
+    val publicationTime: String = "14:00",
+    val postContent: String = "content content content content content",
+    val postContentImage: Int = R.drawable.post_content_image,
+    val statistics: List<StatisticItem> = listOf<StatisticItem>(
+        StatisticItem(
+            type = StatisticItemType.LIKES,
+            count = randomCountInRangeForStatisticItem(10..100)
+        ),
+        StatisticItem(
+            type = StatisticItemType.COMMENTS,
+            count = randomCountInRangeForStatisticItem(10..100)
+        ),
+        StatisticItem(
+            type = StatisticItemType.SHARES,
+            count = randomCountInRangeForStatisticItem(10..100)
+        ),
+        StatisticItem(
+            type = StatisticItemType.SHARES,
+            count = randomCountInRangeForStatisticItem(100..1000)
+        )
+    ),
+)
