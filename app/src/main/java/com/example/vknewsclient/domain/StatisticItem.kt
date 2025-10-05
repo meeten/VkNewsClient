18c4c9192129a385ebdf2c9d3024563df5ae1ddc
meeten <153901120+meeten@users.noreply.github.com>
2025-10-05 19:54:10 +0200

    feat: add StatisticItem data model - Создать StatisticItem дата класс для статистики поста - Определить StatisticItemType с ресурсами значка

---
+++ app/src/main/java/com/example/vknewsclient/domain/StatisticItem.kt
@@ -0,0 +1,15 @@
+package com.example.vknewsclient.domain
+
+import com.example.vknewsclient.R
+
+data class StatisticItem(
+    val type: StatisticItemType,
+    val count: Int,
+)
+
+enum class StatisticItemType(val src: Int) {
+    LIKES(src = R.drawable.ic_like),
+    COMMENTS(src = R.drawable.ic_comment),
+    SHARES(src = R.drawable.ic_share),
+    VIEWS(src = R.drawable.ic_views_count)
+}
