682b0360fa69f7b33ff9a384be086c73ad75c12a
meeten <153901120+meeten@users.noreply.github.com>
2025-10-06 19:11:14 +0200

    feat: enhance HomeViewModel with statistics update functionality

--- app/src/main/java/com/example/vknewsclient/presention/home/HomeViewModel.kt
+++ app/src/main/java/com/example/vknewsclient/presention/home/HomeViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.vknewsclient.domain.FeedPost
+import com.example.vknewsclient.domain.StatisticItem
 
 class HomeViewModel : ViewModel() {
     private val initList = mutableListOf<FeedPost>().apply {
@@ -14,6 +15,40 @@ class HomeViewModel : ViewModel() {
 
     private val _feedPosts = MutableLiveData<List<FeedPost>>(initList)
     val feedPosts: LiveData<List<FeedPost>> get() = _feedPosts
+
+    fun changeStatisticsFeedPost(
+        feedPost: FeedPost,
+        statisticItem: StatisticItem,
+    ) {
+        val modifiedFeedPosts = _feedPosts.value?.toMutableList() ?: mutableListOf()
+        modifiedFeedPosts.replaceAll { oldFeedPost ->
+            if (oldFeedPost.id == feedPost.id) {
+                val newStatistics =
+                    updateCountStatisticItem(feedPost.statistics, statisticItem)
+                oldFeedPost.copy(statistics = newStatistics)
+            } else {
+                oldFeedPost
+            }
+        }
+
+        _feedPosts.value = modifiedFeedPosts
+    }
+
+    private fun updateCountStatisticItem(
+        statistics: List<StatisticItem>,
+        statisticItem: StatisticItem,
+    ): List<StatisticItem> {
+        val modifiedStatistic = statistics.toMutableList()
+        modifiedStatistic.replaceAll { oldStatisticItem ->
+            if (oldStatisticItem.type == statisticItem.type) {
+                oldStatisticItem.copy(count = oldStatisticItem.count + 1)
+            } else {
+                oldStatisticItem
+            }
+        }
+
+        return modifiedStatistic
+    }
 
     fun deleteFeedPost(feedPost: FeedPost) {
         val modifiedFeedPosts = _feedPosts.value?.toMutableList() ?: mutableListOf()
