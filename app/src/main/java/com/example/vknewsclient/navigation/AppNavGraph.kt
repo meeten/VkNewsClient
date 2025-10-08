602671472398e92dc9e03de4cf6a559284371028
meeten <153901120+meeten@users.noreply.github.com>
2025-10-08 19:21:37 +0200

    feat: integrate postNavGraph into AppNavGraph

--- app/src/main/java/com/example/vknewsclient/navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/vknewsclient/navigation/AppNavGraph.kt
@@ -4,18 +4,19 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.vknewsclient.domain.models.FeedPost
+
 
 @Composable
 fun AppNavGraph(
     navController: NavHostController,
     homeContentScreen: @Composable () -> Unit,
+    commentContentScreen: @Composable (FeedPost) -> Unit,
     favoriteContentScreen: @Composable () -> Unit,
     profileContentScreen: @Composable () -> Unit,
 ) {
     NavHost(navController = navController, startDestination = Screen.Home.route) {
-        composable(Screen.Home.route) {
-            homeContentScreen()
-        }
+        postNavGraph(homeContentScreen, commentContentScreen)
 
         composable(Screen.Favorite.route) {
             favoriteContentScreen()
