bee14cd7a3f7ddac470b5c7a23b531daad0b5368
meeten <153901120+meeten@users.noreply.github.com>
2025-10-09 20:22:53 +0200

    refactor: clean up FeedPost data model

--- app/src/main/java/com/example/vknewsclient/domain/models/FeedPost.kt
+++ app/src/main/java/com/example/vknewsclient/domain/models/FeedPost.kt
@@ -9,28 +9,11 @@ private fun randomCountInRangeForStatisticItem(range: IntRange): Int {
 }
 
 data class FeedPost(
-    val id: Int,
-    val publicImage: Int = R.drawable.post_comunity_thumbnail,
-    val publicName: String = "/dev/null",
-    val publicationTime: String = "14:00",
-    val postContent: String = "content content content content content",
-    val postContentImage: Int = R.drawable.post_content_image,
-    val statistics: List<StatisticItem> = listOf<StatisticItem>(
-        StatisticItem(
-            type = StatisticItemType.LIKES,
-            count = randomCountInRangeForStatisticItem(10..100)
-        ),
-        StatisticItem(
-            type = StatisticItemType.COMMENTS,
-            count = randomCountInRangeForStatisticItem(10..100)
-        ),
-        StatisticItem(
-            type = StatisticItemType.SHARES,
-            count = randomCountInRangeForStatisticItem(10..100)
-        ),
-        StatisticItem(
-            type = StatisticItemType.VIEWS,
-            count = randomCountInRangeForStatisticItem(100..1000)
-        )
-    ),
+    val id: String,
+    val publicImageUrl: String,
+    val publicName: String,
+    val publicationTime: String,
+    val postContent: String,
+    val postContentImageUrl: String,
+    val statistics: List<StatisticItem>,
 )
