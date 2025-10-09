02532c63ba0fb58ac4f7d1c11752db43ec5cbca7
meeten <153901120+meeten@users.noreply.github.com>
2025-10-09 21:01:38 +0200

    feat: implement NewsFeedPostMapper for DTO to domain mapping

---
+++ app/src/main/java/com/example/vknewsclient/data/mapper/NewsFeedPostMapper.kt
@@ -0,0 +1,50 @@
+package com.example.vknewsclient.data.mapper
+
+import com.example.vknewsclient.data.model.NewsFeedResponseDto
+import com.example.vknewsclient.domain.models.FeedPost
+import com.example.vknewsclient.domain.models.StatisticItem
+import com.example.vknewsclient.domain.models.StatisticItemType
+import kotlin.math.absoluteValue
+
+class NewsFeedPostMapper {
+
+    fun mapResponseToPosts(responseDto: NewsFeedResponseDto): List<FeedPost> {
+        val result = mutableListOf<FeedPost>()
+
+        val posts = responseDto.newsFeedContent.posts
+        val groups = responseDto.newsFeedContent.groups
+
+        for (post in posts) {
+
+            val group = groups.find { it.id == post.fromId.absoluteValue } ?: break
+            val feedPost = FeedPost(
+                id = post.id,
+                publicImageUrl = group.photoGroupUrl,
+                publicName = group.name,
+                publicationTime = post.date,
+                postContent = post.text,
+                postContentImageUrl = post.attachmentsDto?.lastOrNull()?.photo?.photoUrls?.lastOrNull()?.photoUrl,
+                statistics = listOf<StatisticItem>(
+                    StatisticItem(
+                        type = StatisticItemType.LIKES,
+                        count = post.likesDto.count
+                    ),
+                    StatisticItem(
+                        type = StatisticItemType.COMMENTS,
+                        count = post.commentsDto.count
+                    ),
+                    StatisticItem(
+                        type = StatisticItemType.SHARES,
+                        count = post.reportsDto.count
+                    ),
+                    StatisticItem(
+                        type = StatisticItemType.VIEWS,
+                        count = post.viewsDto.count
+                    )
+                )
+            )
+        }
+
+        return result
+    }
+}
