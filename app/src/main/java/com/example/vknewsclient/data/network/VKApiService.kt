afa234e04161ea67eac2d17ba3ecb9d4d24693f0
meeten <153901120+meeten@users.noreply.github.com>
2025-10-10 19:39:56 +0200

    feat: implement VK API service for posts loading – Добавить интерфейс ApiService с аннотациями Retrofit – Реализовать метод loadPostsByDomain с необходимыми параметрами – Настроить конечную точку API с версией 5.199 и расширенным режимом – Включить токен доступа и домен в качестве параметров запроса

---
+++ app/src/main/java/com/example/vknewsclient/data/network/VKApiService.kt
@@ -0,0 +1,13 @@
+package com.example.vknewsclient.data.network
+
+import com.example.vknewsclient.data.model.NewsFeedResponseDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface VKApiService {
+    @GET("wall.get?v=5.199&extended=true")
+    suspend fun loadPostsByDomain(
+        @Query("access_token") accessToken: String,
+        @Query("domain") domain: String,
+    ): NewsFeedResponseDto
+}
