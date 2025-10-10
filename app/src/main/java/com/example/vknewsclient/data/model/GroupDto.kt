f6cbc228afa5afc99e4c388749e2346713554e49
meeten <153901120+meeten@users.noreply.github.com>
2025-10-10 20:36:44 +0200

    fix: restore id field in GroupDto model

--- app/src/main/java/com/example/vknewsclient/data/model/GroupDto.kt
+++ app/src/main/java/com/example/vknewsclient/data/model/GroupDto.kt
@@ -3,6 +3,7 @@ package com.example.vknewsclient.data.model
 import com.google.gson.annotations.SerializedName
 
 class GroupDto(
+    @SerializedName("id") val id: Int,
     @SerializedName("name") val name: String,
     @SerializedName("photo_200") val photoGroupUrl: String,
 )
