f9e9bfaadbc25f0f0ec7ac8845e5103825770d6d
meeten <153901120+meeten@users.noreply.github.com>
2025-10-10 20:38:34 +0200

    fix: update field types in FeedPost

--- app/src/main/java/com/example/vknewsclient/domain/models/FeedPost.kt
+++ app/src/main/java/com/example/vknewsclient/domain/models/FeedPost.kt
@@ -1,10 +1,10 @@
 package com.example.vknewsclient.domain.models
 
 data class FeedPost(
-    val id: String,
+    val id: Int,
     val publicImageUrl: String,
     val publicName: String,
-    val publicationTime: String,
+    val publicationTime: Int,
     val postContent: String,
     val postContentImageUrl: String?,
     val statistics: List<StatisticItem>,
