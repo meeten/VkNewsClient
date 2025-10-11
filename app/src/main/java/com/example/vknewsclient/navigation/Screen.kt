d61aa27e3e43465ca25ae0393197917c44a89b6e
meeten <153901120+meeten@users.noreply.github.com>
2025-10-11 17:35:49 +0200

    feat: implement FeedPost object serialization in navigation

--- app/src/main/java/com/example/vknewsclient/navigation/Screen.kt
+++ app/src/main/java/com/example/vknewsclient/navigation/Screen.kt
@@ -1,28 +1,35 @@
 package com.example.vknewsclient.navigation
 
+import android.net.Uri
 import com.example.vknewsclient.domain.models.FeedPost
+import com.google.gson.Gson
 
 sealed class Screen(val route: String) {
     object Comment : Screen(COMMENT_ROUTE) {
         const val COMMENT_ROUTE_WITH_ARGS = "comment"
 
         fun createRouteWithArgs(feedPost: FeedPost): String {
-            return "$COMMENT_ROUTE_WITH_ARGS/${feedPost.id}"
+            val feedPostGson = Gson().toJson(feedPost)
+            return "$COMMENT_ROUTE_WITH_ARGS/${feedPostGson.encode()}"
         }
-    }
-
-    companion object {
-        const val KEY_FEED_POST_ID = "feedPostId"
-
-        const val HOME_ROUTE = "home"
-        const val NEWS_ROUTE = "news"
-        const val COMMENT_ROUTE = "comment/{$KEY_FEED_POST_ID}"
-        const val FAVORITE_ROUTE = "favorite"
-        const val PROFILE_ROUTE = "profile"
     }
 
     object Home : Screen(HOME_ROUTE)
     object News : Screen(NEWS_ROUTE)
     object Favorite : Screen(FAVORITE_ROUTE)
     object Profile : Screen(PROFILE_ROUTE)
+
+    companion object {
+        const val KEY_FEED_POST = "feedPost"
+
+        const val HOME_ROUTE = "home"
+        const val NEWS_ROUTE = "news"
+        const val COMMENT_ROUTE = "comment/{$KEY_FEED_POST}"
+        const val FAVORITE_ROUTE = "favorite"
+        const val PROFILE_ROUTE = "profile"
+    }
+}
+
+fun String.encode(): String {
+    return Uri.encode(this)
 }
