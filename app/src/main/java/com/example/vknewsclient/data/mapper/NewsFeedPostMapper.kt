82027efc4d48a21d0fdcf7b505d455a760402960
meeten <153901120+meeten@users.noreply.github.com>
2025-10-12 21:10:41 +0200

    feat: add timestamp to date conversion in NewsFeedPostMapper

--- app/src/main/java/com/example/vknewsclient/data/mapper/NewsFeedPostMapper.kt
+++ app/src/main/java/com/example/vknewsclient/data/mapper/NewsFeedPostMapper.kt
@@ -4,6 +4,9 @@ import com.example.vknewsclient.data.model.NewsFeedResponseDto
 import com.example.vknewsclient.domain.models.FeedPost
 import com.example.vknewsclient.domain.models.StatisticItem
 import com.example.vknewsclient.domain.models.StatisticItemType
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.util.Date
 import kotlin.math.absoluteValue
 
 class NewsFeedPostMapper {
@@ -21,7 +24,7 @@ class NewsFeedPostMapper {
                 id = post.id,
                 publicImageUrl = group.photoGroupUrl,
                 publicName = group.name,
-                publicationTime = post.date,
+                publicationTime = convertTimestampInDate(post.date),
                 postContent = post.text,
                 postContentImageUrl = post.attachmentsDto?.lastOrNull()?.photo?.photoUrls?.lastOrNull()?.photoUrl
                     ?: continue,
@@ -50,4 +53,10 @@ class NewsFeedPostMapper {
 
         return result
     }
+}
+
+private fun convertTimestampInDate(timestamp: Int): String {
+    val date = Date(timestamp * 1000L)
+    val format = SimpleDateFormat("d MMM HH:mm")
+    return format.format(date).replace(".", "")
 }
