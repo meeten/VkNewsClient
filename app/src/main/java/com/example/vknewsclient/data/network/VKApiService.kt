3e6dc31bdccb35f466bd58fa0900889c0929c2d6
meeten <153901120+meeten@users.noreply.github.com>
2025-10-13 20:40:48 +0200

    feat: add post count parameter to wall.get API call

--- app/src/main/java/com/example/vknewsclient/data/network/VKApiService.kt
+++ app/src/main/java/com/example/vknewsclient/data/network/VKApiService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface VKApiService {
-    @GET("wall.get?v=5.199&extended=true")
+    @GET("wall.get?v=5.199&extended=true&count=50")
     suspend fun loadPostsByDomain(
         @Query("access_token") accessToken: String,
         @Query("domain") domain: String,
