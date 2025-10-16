e38f5859141cf0d073e6f2463f508b04005174b8
meeten <153901120+meeten@users.noreply.github.com>
2025-10-16 20:23:39 +0200

    feat: add loading UI for pagination

--- app/src/main/java/com/example/vknewsclient/presention/home/HomeScreen.kt
+++ app/src/main/java/com/example/vknewsclient/presention/home/HomeScreen.kt
@@ -1,17 +1,23 @@
 package com.example.vknewsclient.presention.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.SwipeToDismissBox
-import androidx.compose.material3.SwipeToDismissBoxValue
-import androidx.compose.material3.rememberSwipeToDismissBoxState
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.vknewsclient.domain.models.FeedPost
 import com.example.vknewsclient.domain.state.NewsFeedScreenState
 import com.example.vknewsclient.presention.home.news.PostCard
+import com.example.vknewsclient.ui.theme.DarkBlue
 
 @Composable
 fun HomeScreen(modifier: Modifier, onCommentClickListener: (FeedPost) -> Unit) {
@@ -22,7 +28,12 @@ fun HomeScreen(modifier: Modifier, onCommentClickListener: (FeedPost) -> Unit) {
     val currentState = newsFeedScreenState
     when (currentState) {
         is NewsFeedScreenState.Posts -> {
-            NewsFeed(currentState.feedPosts, viewModel, onCommentClickListener)
+            NewsFeedContent(
+                currentState.feedPosts,
+                currentState.isLoadingNextPosts,
+                viewModel,
+                onCommentClickListener,
+            )
         }
 
         is NewsFeedScreenState.Initial -> {
@@ -32,38 +43,46 @@ fun HomeScreen(modifier: Modifier, onCommentClickListener: (FeedPost) -> Unit) {
 }
 
 @Composable
-private fun NewsFeed(
+private fun NewsFeedContent(
     feedPosts: List<FeedPost>,
+    isLoadingNextPosts: Boolean,
     viewModel: HomeViewModel,
     onCommentClickListener: (FeedPost) -> Unit,
 ) {
-    LazyColumn() {
-        items(items = feedPosts, key = { it.id }) { feedPost ->
-            val swipeToDismissBoxState = rememberSwipeToDismissBoxState(
-                confirmValueChange = {
-                    if (it == SwipeToDismissBoxValue.EndToStart) {
-                        viewModel.deleteFeedPost(feedPost)
-                        true
-                    }
 
-                    false
-                })
+    LazyColumn(
+        contentPadding = PaddingValues(
+            start = 8.dp,
+            end = 8.dp,
+            top = 8.dp,
+            bottom = 100.dp
+        )
+    ) {
+        items(items = feedPosts) { feedPost ->
+            PostCard(
+                feedPost = feedPost,
+                onLikeClickListener = { viewModel.changeLikeStatus(feedPost) },
+                onCommentClickListener = { onCommentClickListener(feedPost) },
+                onShareClickListener = { viewModel.incrementStatistics(feedPost, it) },
+                onViewClickListener = { viewModel.incrementStatistics(feedPost, it) }
+            )
+        }
 
-            SwipeToDismissBox(
-                state = swipeToDismissBoxState,
-                backgroundContent = {},
-                modifier = Modifier.animateItem(),
-                enableDismissFromStartToEnd = false,
-            ) {
-                PostCard(
-                    feedPost = feedPost,
-                    onLikeClickListener = { viewModel.changeLikeStatus(feedPost) },
-                    onCommentClickListener = { onCommentClickListener(feedPost) },
-                    onShareClickListener = { viewModel.incrementStatistics(feedPost, it) },
-                    onViewClickListener = { viewModel.incrementStatistics(feedPost, it) }
-                )
+        item {
+            if (isLoadingNextPosts) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator(color = DarkBlue)
+                }
+            } else {
+                SideEffect {
+                    viewModel.loadNextPosts()
+                }
             }
         }
     }
 }
-
