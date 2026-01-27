bd7fdb8cfa626dde00fc5b288aa0031b8aa9ccfd
meeten <153901120+meeten@users.noreply.github.com>
2026-01-27 19:25:08 +0200

    build: add dagger2 dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("kotlin-kapt")
     id("vkid.manifest.placeholders")
 }
 
@@ -64,6 +65,8 @@ android {
 
 dependencies {
 
+    implementation(libs.dagger)
+    kapt(libs.dagger.compiler)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
